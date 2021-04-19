5a2b07956e05069432f8d3d16a7f709c6ea3b3c5
Anirudh Menon <senseianirudh28@gmail.com>
2021-04-20 01:01:32 +0530

    Small change

--- flutter_test_app/lib/main.dart
+++ flutter_test_app/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {
- runApp(MyApp()); //runs app with MyAPP as core widget. Flutter auto calls build funct
-}
+// void main() {
+//  runApp(MyApp()); //runs app with MyAPP as core widget. Flutter auto calls build funct
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(home: Text('Hello!')); //returns an object of widget class and calls its constructor with a Text widget 
   }
