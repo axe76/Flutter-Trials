69ab08541de60fef4d836f0c75a731124769c590
Anirudh Menon <senseianirudh28@gmail.com>
2021-04-24 01:39:40 +0530

    Styling of transactions done

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,6 +22,12 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: "t1",cost: 72.99, date: DateTime.now(), name: "Shoes"),
+    Transaction(id: "t2",cost: 16.99, date: DateTime.now(), name: "Groceries"),
+  ];
+
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -26,6 +35,8 @@ class MyHomePage extends StatelessWidget {
         title: Text("Flutter App"),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
             width: double.infinity,
@@ -34,7 +45,35 @@ class MyHomePage extends StatelessWidget {
               child: Text("CHART",style: TextStyle(color: Colors.white),),
               elevation: 5,),
           ),
-          Card(child: Text("List of TX"),)
+          Column(children: transactions.map((tx) {
+            return Card(
+              elevation: 4,
+              child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                  decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 2)),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${tx.cost}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple,
+                      fontSize: 20,
+                    ),),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text(
+                    tx.name,
+                    style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                  Text(
+                    DateFormat.yMMMMd().format(tx.date),
+                    style: TextStyle(color: Colors.grey),)
+                ],)
+              ],),);
+          } ).toList(),)
         ],)
     );
   }
