610c9c6ad1092b2e20ed56dc991e94476c0df851
Anirudh Menon <senseianirudh28@gmail.com>
2021-05-29 16:12:35 +0530

    Added google maps static

--- great_places_app/lib/screens/add_place_screen.dart
+++ great_places_app/lib/screens/add_place_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
 import '../providers/great_places.dart';
 
 class AddPlacesScreen extends StatefulWidget {
@@ -46,7 +47,8 @@ class _AddPlacesScreenState extends State<AddPlacesScreen> {
                   ),
                   SizedBox(height:10),
                   ImageInput(_selectImage),
-
+                  SizedBox(height:10),
+                  LocationInput(),
                 ],
                 ),
             ),
