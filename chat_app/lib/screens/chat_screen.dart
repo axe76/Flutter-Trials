c4099a9665a3bdad2761913b632efb34794c0d49
Anirudh Menon <senseianirudh28@gmail.com>
2021-06-04 14:11:23 +0530

    Added push notif and user image in msg

--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import '../widgets/chat/messages.dart';
 import '../widgets/chat/new_message.dart';
 
 
-class ChatScreen extends StatelessWidget {
-  
+class ChatScreen extends StatefulWidget {
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
 
   @override
   Widget build(BuildContext context) {
--- chat_app/lib/widgets/chat/message_bubble.dart
+++ chat_app/lib/widgets/chat/message_bubble.dart
@@ -7,46 +7,60 @@ class MessageBubble extends StatelessWidget {
   final bool isMe;
   final key;
   final String username;
+  final String userImage;
 
-  MessageBubble(this.message,this.username,this.isMe,{this.key});
+  MessageBubble(this.message,this.username,this.userImage,this.isMe,{this.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: isMe?MainAxisAlignment.end:MainAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: isMe? Colors.grey[300]: Theme.of(context).accentColor,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(12),
-              topRight: Radius.circular(12),
-              bottomLeft: !isMe?Radius.circular(0):Radius.circular(12),
-              bottomRight: isMe?Radius.circular(0):Radius.circular(12)
-            )
+        Row(
+        mainAxisAlignment: isMe?MainAxisAlignment.end:MainAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: isMe? Colors.grey[300]: Theme.of(context).accentColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+                bottomLeft: !isMe?Radius.circular(0):Radius.circular(12),
+                bottomRight: isMe?Radius.circular(0):Radius.circular(12)
+              )
+            ),
+            width: 140,
+            padding: EdgeInsets.symmetric(vertical: 10,horizontal: 16),
+            margin: EdgeInsets.symmetric(vertical: 14,horizontal: 8),
+            child: Column(
+              crossAxisAlignment: isMe?CrossAxisAlignment.end:CrossAxisAlignment.start,
+              children: [
+                Text(
+                      username,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: isMe? Colors.black:Theme.of(context).accentTextTheme.title.color,
+                        )
+                ),
+                Text(
+                  message,
+                  style: TextStyle(color: isMe? Colors.black:Theme.of(context).accentTextTheme.title.color),
+                  textAlign: isMe? TextAlign.end:TextAlign.start,
+                ),
+              ],
+            ),
           ),
-          width: 140,
-          padding: EdgeInsets.symmetric(vertical: 10,horizontal: 16),
-          margin: EdgeInsets.symmetric(vertical: 4,horizontal: 8),
-          child: Column(
-            crossAxisAlignment: isMe?CrossAxisAlignment.end:CrossAxisAlignment.start,
-            children: [
-              Text(
-                    username,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: isMe? Colors.black:Theme.of(context).accentTextTheme.title.color,
-                      )
-              ),
-              Text(
-                message,
-                style: TextStyle(color: isMe? Colors.black:Theme.of(context).accentTextTheme.title.color),
-                textAlign: isMe? TextAlign.end:TextAlign.start,
-              ),
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
+    Positioned(//allows positioning of widgets in stack with respect to widget below in stack, in 2d space
+      top: 0,
+      left: isMe? null:120,
+      right: isMe?120:null,
+      child: CircleAvatar(
+        backgroundImage: NetworkImage(userImage),
+      )
+    )  
+    ],
+    overflow: Overflow.visible,//allows widgets in stack that are clipped off to be visible
     );
   }
 }
